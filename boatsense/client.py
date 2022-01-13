c38699610eb3be7b2d2a95b504e436d3d102c5ca
Neal Stephenson <neal@bakerst.org>
2022-01-13 22:21:37 +0000

    client fixes

--- boatsense/client.py
+++ boatsense/client.py
@@ -4,6 +4,8 @@ from board import I2C
 from datetime import datetime, timezone
 from logging import getLogger
 from schedule import every, run_pending
+from socket import AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_REUSEADDR, SO_BROADCAST
+from socket import socket
 from sqlalchemy.orm import Session
 from time import sleep
 
@@ -19,6 +21,9 @@ class Client(object):
     def __init__(self, db: Session):
         self.db = db
         i2c = I2C()
+        self.server = socket(AF_INET, SOCK_DGRAM)
+        self.server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
         self.sensors = {}
         self.sensors['bme280'] = sensors.BME280(i2c)
         self.sensors['lsm9ds1'] = sensors.LSM9DS1(i2c)
@@ -36,6 +41,8 @@ class Client(object):
             if data:
                 asat = datetime.now(tz=timezone.utc)
                 crud.add_sensor(self.db, name, asat, data)
+                msg = '{{"{}": {}}}'.format(name, data.json()).encode('utf-8')
+                self.server.sendto(msg, CFG.udp_addr)
         else:
             if name == 'upload':
                 self.upload()
