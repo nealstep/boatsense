0ac2c35fea658c3301bd8fb2e6205ad44ad3b8e7
Neal Stephenson <neal@bakerst.org>
2022-01-13 17:22:35 -0500

    test client for udp

---
+++ test/test_udp_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from json import loads
+from socket import AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
+from socket import socket
+
+client = socket(AF_INET, SOCK_DGRAM)
+client.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+client.bind(("", 22001))
+while True:
+    data, addr = client.recvfrom(1024)
+    values = loads(data.decode("utf-8"))
+    print("msg ({}): {}".format(addr, values))
+    if 'gps' in values:
+        print(values['gps']['lat'])
